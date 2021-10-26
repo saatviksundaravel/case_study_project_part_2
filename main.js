2814bbc6a61e4352d3c8a993a2196e8cbe9ac515
saatviksundaravel <77335691+saatviksundaravel@users.noreply.github.com>
2021-10-26 15:37:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+
+}
+
+function setup(){
+        canvas = createCanvas(300, 300)
+        canvas.center();
+        video = createCapture(VIDEO);
+        video.size(300, 300);
+        video.hide();
+    
+        poseNet = ml5.poseNet(video, modelLoaded);
+        poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 15;
+    }
